29da08a35a24c50c95881fd864f253ecb8c4f852
Ryan Johnson <ryan.johnson@hellosign.com>
2017-12-13 10:35:30 -0800

    initial commit

---
+++ lib/supervised_wkhtmltopdf.ex
@@ -0,0 +1,18 @@
+defmodule SupervisedWkhtmltopdf do
+  @moduledoc """
+  Documentation for SupervisedWkhtmltopdf.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SupervisedWkhtmltopdf.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/supervised_wkhtmltopdf_test.exs
@@ -0,0 +1,8 @@
+defmodule SupervisedWkhtmltopdfTest do
+  use ExUnit.Case
+  doctest SupervisedWkhtmltopdf
+
+  test "greets the world" do
+    assert SupervisedWkhtmltopdf.hello() == :world
+  end
+end
